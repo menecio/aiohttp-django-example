f8ad621e3589d01513934d820763c4f07ffc298c
Aristóbulo Meneses <aristobulo@gmail.com>
2019-09-06 17:54:09 +0200

    Implement list and detail views

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
+import json
+
 from aiohttp import web
 from django.apps import apps
 from django.core.serializers import serialize
+from django.core.serializers.json import DjangoJSONEncoder
 
 from mymoviedb.utils import database_sync_to_async
 
@@ -8,7 +11,7 @@ routes = web.RouteTableDef()
 
 
 @routes.view('/movies')
-class MoviesView(web.View):
+class MoviesListView(web.View):
     model = 'movies.Movie'
 
     async def get_queryset(self):
@@ -18,4 +21,24 @@ class MoviesView(web.View):
     async def get(self):
         queryset = await self.get_queryset()
         text = serialize('json', queryset)
+        return web.json_response(text=text)
+
+
+@routes.view('/movies/{pk}')
+class MoviesDetailView(web.View):
+    model = 'movies.Movie'
+
+    async def get_queryset(self, **kwargs):
+        model = apps.get_model(self.model)
+        return await database_sync_to_async(model.objects.filter)(**kwargs)
+
+    async def get(self):
+        pk = self.request.match_info['pk']
+        queryset = await self.get_queryset(pk=pk)
+        movie = queryset.values().first()
+
+        if not movie:
+            return web.json_response(data={'message': 'Not found'}, status=404)
+
+        text = json.dumps(movie, cls=DjangoJSONEncoder)
         return web.json_response(text=text)
