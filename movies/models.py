21bec2611dbc81cad17954934c2fc8fb85274910
Aristóbulo Meneses <aristobulo@gmail.com>
2019-09-06 18:05:41 +0200

    I shouldn't modify migrations but this is an example.

--- movies/models.py
+++ movies/models.py
@@ -7,6 +7,10 @@ class Movie(models.Model):
     rating = models.PositiveSmallIntegerField()
     duration = models.DurationField()
     director = models.CharField(max_length=512)
+    genre = models.CharField(max_length=16)
 
     class Meta:
         app_label = 'movies'
+
+    def __str__(self):
+        return f'{self.title} - {self.release_date.year}'
