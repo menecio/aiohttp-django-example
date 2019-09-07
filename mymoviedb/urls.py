482675784e22eab030f69ebf755f3b68a125d0c1
Aristóbulo Meneses <aristobulo@gmail.com>
2019-09-07 15:44:23 +0200

    Serve django app with gunicorn

--- mymoviedb/urls.py
+++ mymoviedb/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import  staticfiles_urlpatterns
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
